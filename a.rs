06b75d0af41bdaf7f8e15da55a681e8ef8c78dc7
毛庭峰 <mtingfeng@gmail.com>
2020-12-12 23:56:04 +0800

    feat(5.1): self参数为Box指针类型

--- a.rs
+++ a.rs
@@ -1,27 +1,23 @@
 
 trait Shape {
-  fn area(self: &Self) -> f64;
-  // 等价于上面的
-  // fn area(&self) -> f64;
+  fn area(self: Box<Self>) -> f64;
 }
+
 struct Circle {
   radius: f64,
 }
 
 impl Shape for Circle {
-  fn area(&self) -> f64 {
+  fn area(self: Box<Self>) -> f64 {
     std::f64::consts::PI * self.radius * self.radius
   }
 }
 
-impl Circle {
-  fn get_radius(&self) -> f64 {
-    self.radius
-  }
-}
 
 fn main() {
-  let c = Circle { radius: 2f64 };
-  println!("The area is {}", c.get_radius());
+  // let c = Circle { radius: 2f64 };
+  // c.area();
+  let b = Box::new(Circle { radius: 4f64 });
+  b.area();
 }
 
