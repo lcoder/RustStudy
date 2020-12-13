8cf9b35ed095a7393c2e2bbd9450d054d31d4c7a
毛庭峰 <mtingfeng@gmail.com>
2020-12-13 17:17:59 +0800

    5.7(error): trait别名

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-#[derive(Copy, Clone, Default, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-
-struct Foo {
-  data: i32,
+pub trait Service {
+  type Request;
+  type Response;
+  type Error;
+  type Future;
+  fn call(&self, req: Self::Request) -> Self::Future;
 }
 
+trait HttpService = Service<
+  Request = http::Request,
+  Response = http::Response,
+  Error = http::Error
+>;
+
+
 fn main() {
-  let v1 = Foo { data: 0 };
-  let v2 = v1;
-  println!("{:?}", v2);
+  
 }
 
