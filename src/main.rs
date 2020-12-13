42f47fa94304bfae1a94b6bb73013555baf76a63
毛庭峰 <mtingfeng@gmail.com>
2020-12-13 20:38:38 +0800

    5.8: Display和Debug

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-pub trait Service {
-  type Request;
-  type Response;
-  type Error;
-  type Future;
-  fn call(&self, req: Self::Request) -> Self::Future;
+use std::fmt::{Display, Formatter, Error};
+
+#[derive(Debug)]
+struct T {
+  field1: i32,
+  field2: i32,
 }
 
-trait HttpService = Service<
-  Request = http::Request,
-  Response = http::Response,
-  Error = http::Error
->;
-
+impl Display for T {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "{{ field1: {}, field2: {} }}", self.field1, self.field2)
+  }
+}
 
 fn main() {
-  
+  let var = T { field1: 1, field2: 2 };
+  println!("{}", var);
+  println!("{:?}", var);
+  println!("{:#?}", var);
 }
 
