08ceb14533a47310904bd5ab779c70b90e5bb653
毛庭峰 <mtingfeng@gmail.com>
2020-12-13 21:04:31 +0800

    5.8.3: Sized.Rust中但凡能在编译阶段确定大小的类型,都满足Sized约束

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#[lang = "sized"]
+#[rustc_on_unimplemented = "`{Self}` does not have a constant size known at compile-time"]
+#[fundamental] // for Default,for example,which required taht `[T]: !Default` be evaluatable
+pub trait Sized {
+  // Empty.
+}
+
 
 fn main() {
-  let int_vec = [1_i32, 2, 3];
-  let biggest_int = int_vec.iter().max();
-
-  let float_vec = [1.0_f32, 2.0, 3.0];
-  let biggest_float = float_vec.iter().max();
+  
 }
 
