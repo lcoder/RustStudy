2bbfbe6215be4019396edad54eeedef9a3ef59ae
毛庭峰 <mtingfeng@gmail.com>
2020-12-13 11:11:12 +0800

    2.3: struct初始化,使用另外一个struct部分成员

--- a.rs
+++ a.rs
@@ -1,15 +1,18 @@
+#[derive(Debug)]
 
-struct T(i32);
+struct Point3d {
+  x: i32,
+  y: i32,
+  z: i32,
+}
 
-impl T {
-  fn func(this: &Self) {
-    println!("value {}", this.0);
-  }
+fn default() -> Point3d {
+  Point3d { x: 0, y: 0, z: 0 }
 }
 
 fn main() {
-  let x = T(42);
-
-  T::func(&x);
+  let origin = Point3d { x: 5, ..default() };
+  let point = Point3d { z: 1, x: 2, ..origin };
+  println!("origin origin = {:?} point = {:?}", origin, point);
 }
 
