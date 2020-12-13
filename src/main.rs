5b343ad2d9851d792c593ade5d1c608b7f9e3c11
毛庭峰 <mtingfeng@gmail.com>
2020-12-13 20:39:53 +0800

    5.8: 浮点数不具备全序关系

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
-use std::fmt::{Display, Formatter, Error};
-
-#[derive(Debug)]
-struct T {
-  field1: i32,
-  field2: i32,
-}
-
-impl Display for T {
-  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-    write!(f, "{{ field1: {}, field2: {} }}", self.field1, self.field2)
-  }
-}
 
 fn main() {
-  let var = T { field1: 1, field2: 2 };
-  println!("{}", var);
-  println!("{:?}", var);
-  println!("{:#?}", var);
+  let nan = std::f32::NAN;
+  let x = 1.0f32;
+
+  println!("{}", nan < x);
+  println!("{}", nan > x);
+  println!("{}", nan == x);
 }
 
