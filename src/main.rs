0cca47a71a71aa6385605a4f1388f53406dca7d9
毛庭峰 <mtingfeng@gmail.com>
2020-12-13 20:51:50 +0800

    5.8.2: 全序,偏序

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 
 fn main() {
-  let nan = std::f32::NAN;
-  let x = 1.0f32;
+  let int_vec = [1_i32, 2, 3];
+  let biggest_int = int_vec.iter().max();
 
-  println!("{}", nan < x);
-  println!("{}", nan > x);
-  println!("{}", nan == x);
+  let float_vec = [1.0_f32, 2.0, 3.0];
+  let biggest_float = float_vec.iter().max();
 }
 
