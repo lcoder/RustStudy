70f417a84f0ed0594d3b80e8603237c05f27ca01
毛庭峰 <mtingfeng@gmail.com>
2020-12-14 23:44:41 +0800

    6.1: 数组索引访问

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 
 fn main() {
-  let mut xs: [i32; 5] = [1, 2, 3, 4, 5];
-  let ys: [i32; 5] = [6, 7, 8, 9, 10];
-
-  xs = ys;
-
-  println!("xs = {:?}; ys = {:?}", xs, ys);
+  let v: [i32; 5] = [1,2,3,4,5];
+  let x = v[0] + v[1];
+  println!("x={}", x);
 }
 
