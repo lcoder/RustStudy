8029cf8abe1143ddb2b9d7be876ee7b0e74282b7
毛庭峰 <mtingfeng@gmail.com>
2020-12-15 22:54:29 +0800

    6.1.5: Range和数组搭配使用

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::iter::Iterator;
-use std::ops::Range;
+
+fn print_slice(arr: &[i32]) {
+  println!("Length: {}", arr.len());
+
+  for item in arr {
+    print!("{}\t", item);
+  }
+
+  println!("");
+}
+
 
 fn main() {
-  let r = 1..10;
-  for i in r {
-    print!("{:?}\t", i);
-  } 
-  let r = Range { start: 1, end: 10 };
-  for i in r {
-    print!("{:?}\t", i)
-  }
+  let arr: [i32; 5] = [1,2,3,4,5];
+  print_slice(&arr[..]);   // full range
 
-  let r = (1i32..11).rev().map(|i| i * 10);
+  let slice = &arr[2..];   // rangeFrom
+  print_slice(slice);
 
-  for i in r {
-    print!("{:?}\t", i);
-  }
+  let slice2 = &arr[..2];  // RangeTo
+  print_slice(slice2);
 }
 
