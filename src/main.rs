94c0675efc88237d98042fdecb987285ec1b1b55
毛庭峰 <mtingfeng@gmail.com>
2020-12-15 23:26:54 +0800

    6.1.6: 数组推荐使用迭代器

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 
 
 fn main() {
-  let v = [10i32, 20, 30, 40, 50];
+  use std::iter::Iterator;
 
-  let first = v.get(0);
-  let tenth = v.get(10);
+  let v = &[10i32, 20, 30, 40, 50];
 
-  println!("{:#?}, {:#?}", first, tenth);
+  // 同时需要index和内部元素的值，调用enumerate()
+  for (index, value) in v.iter().enumerate() {
+    println!("{} {}", index, value);
+  }
+
+  let item = v.iter().filter( |&x| *x % 2 == 0 ).nth(2);
+
+  println!("{:?}", item);
 }
 
