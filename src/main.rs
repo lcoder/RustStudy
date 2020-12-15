e2bc1d4ea05489d630d5a33f3a2c545bdb7a0975
毛庭峰 <mtingfeng@gmail.com>
2020-12-15 23:42:56 +0800

    6.2.1: 字符串-&str

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 
 
 fn main() {
-  use std::iter::Iterator;
+  let greeting: &str = "Hello";
 
-  let v = &[10i32, 20, 30, 40, 50];
+  let substr: &str = &greeting[2..];
 
-  // 同时需要index和内部元素的值，调用enumerate()
-  for (index, value) in v.iter().enumerate() {
-    println!("{} {}", index, value);
-  }
+  println!("{}", substr);
 
-  let item = v.iter().filter( |&x| *x % 2 == 0 ).nth(2);
-
-  println!("{:?}", item);
+  println!("Size of pointer: {}", std::mem::size_of::<*const ()>());
+  println!("Size of &str : {}", std::mem::size_of::<&str>());
 }
 
