f9a139897c406fcefe373c6d4589156b84ba80ed
毛庭峰 <mtingfeng@gmail.com>
2020-12-17 23:38:07 +0800

    7.2.3: match也是表达式

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-
-fn main() {
-  let x = (1, 2, 3);
-  // let (a, _, _) = x;
-
-  let (a, ..) = x;
-
-  // let (a, .., b) = x;
-
-  println!("{}", a);
+enum Direction {
+  East,
+  West,
+  South,
+  North,
 }
 
+fn direction_to_int(x: Direction) -> i32 {
+  match x {
+    Direction::East => 10,
+    Direction::West => 20,
+    Direction::South => 30,
+    Direction::North => 40,
+  }
+}
+
+fn main() {
+  let x = Direction::West;
+  let s = direction_to_int(x);
+  println!("{}", s);
+}
